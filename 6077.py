cb1249e54db7d5f5c3ee1802c743bfebe2486214
Rsujung <vosejtnwjd19@gmail.com>
2024-04-17 18:07:56 +0900

    Correct : 6077
    짝수의 합 구하기 for, while 소스코드

---
+++ 6077.py
@@ -0,0 +1,17 @@
+'''
+for문
+c = int(input())
+sum=0
+for i in range(c+1):
+    if i%2==0:
+        sum += i
+print(sum)
+'''
+
+# while문
+i,result = 2,0
+a = int(input())
+while (i<=a):
+    result+=i
+    i+=2
+print(result)
